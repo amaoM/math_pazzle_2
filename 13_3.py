3d466673d7c34f0f5072f48c25c20b9db1ced5b8
amaoM <chapa0106@gmail.com>
2018-12-15 11:33:08 +0900

    Edit knowledges about No.13

--- 13_3.py
+++ 13_3.py
@@ -1,9 +1,10 @@
-M = 5
-N = 3
+M = 9
+N = 5
 
 
 def search(seq, used, n):
     if n == 0:
+        print(seq)
         result = 1
         cnt = seq.count(0)
         for i in range(1, cnt + 1):
@@ -14,12 +15,12 @@ def search(seq, used, n):
         new_seq = seq[0:i+1]
         new_seq.reverse()
         new_seq.extend(seq[i+1:])
-        print(i, seq, bin(used), bin(1 << i))
-        # ビット演算の意味が理解できない
         if seq[i] == 0 and used & (1 << i) == 0:
+            print(i, seq, new_seq)
             new_seq[0] = i + 1
             cnt += search(new_seq, used | (1 << i), n - 1)
         elif seq[i] == i + 1:
+            print(i, seq, new_seq)
             cnt += search(new_seq, used, n - 1)
     return cnt
 
