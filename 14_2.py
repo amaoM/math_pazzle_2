2e9353864a771c361544a8f3718ff51c9e87ced1
amaoM <chapa0106@gmail.com>
2018-12-18 23:42:40 +0900

    Add an other pattern of No.14

--- 14_2.py
+++ 14_2.py
@@ -1,38 +1,20 @@
-def combinations(l, i, r):
-    if i == 0:
-        return [r]
-    ll = l[::]
-    res = []
-    for k in range(len(l)):
-        rr = r[::]
-        rr.append(l[k])
-        ll.remove(l[k])
-        res.extend(combinations(ll, i - 1, rr))
-    return res
-
-
 def main():
     M = 45678
-    cnt = M
     money_list = [10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1]
-    for i in range(len(money_list), 0, -1):
-        combs = combinations(money_list, i, [])
-        for ml in combs:
-            rest = M
-            for money in ml:
-                rest -= money
-            if 0 > rest:
-                continue
-            c = len(ml)
-            for money in ml:
-                r = rest // money
-                c += r
-                rest -= r * money
-            cnt = min(cnt, c)
-        if cnt < M:
-            break
-    print(cnt)
+    used = [0 for _ in range(len(money_list))]
+    rest = M
+    for i, money in enumerate(money_list):
+        if rest > money:
+            used[i] = 1
+            rest -= money
+    for i, money in enumerate(money_list):
+        used[i] += rest // money
+        rest %= money
+    for i in range(len(money_list) - 1):
+        if used[i] == 0 and money_list[i + 1] * used[i + 1] >= money_list[i]:
+            used[i] = 1
+            used[i + 1] -= money_list[i] // money_list[i + 1]
 
 
-if __name__ in '__main__':
+if __name__ == '__main__':
     main()
