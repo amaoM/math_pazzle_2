bbea6672000e253e5e4a024f77be9e51c6fd53a2
amaoM <chapa0106@gmail.com>
2018-12-24 17:04:37 +0900

    [WIP] No.23

---
+++ 23.py
@@ -0,0 +1,34 @@
+def div(x, y, memo):
+    if (x, y) in memo:
+        return memo[(x, y)]
+    if x == 0:
+        return 1
+    if 0 > x:
+        return 0
+    cnt = 0
+    if 5 >= x >= 2:
+        cnt += 1
+    for xx in range(2, x // 2 + 1):
+        cnt += div(x - xx * 2, y, memo)
+    memo[(x, y)] = cnt
+    return cnt
+
+
+def main():
+    N = 100
+    cnt = 0
+
+    for x in range(2, N // 2 + 1):
+        if N % x != 0:
+            continue
+        y = N // x
+        if x == y:
+            continue
+
+        cnt += div(x, y, {})
+
+    print(cnt)
+
+
+if __name__ == '__main__':
+    main()
