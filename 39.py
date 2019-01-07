c9051837de6e65cfa489f29087398924a6c5954f
hiamao <chapa0106@gmail.com>
2019-01-07 23:27:35 +0900

    Add No.39

--- 39.py
+++ 39.py
@@ -1,5 +1,18 @@
+def check(unused, once, neighbor, memo):
+    if (unused, once, neighbor) in memo:
+        return memo[(unused, once, neighbor)]
+    cnt = 0
+    if unused > 0:
+        cnt += check(unused - 1, once + neighbor, 1, memo)
+    if once > 0:
+        cnt += once * check(unused, once - 1 + neighbor, 0, memo)
+    memo[(unused, once, neighbor)] = cnt
+    return cnt
+
+
 def main():
-    pass
+    N = 11
+    print(check(N, 0, 0, {(0, 0, 0): 1}))
 
 
 if __name__ == '__main__':
